52ac827fe9c8aaeac33389961550d3caf113b803
Ahmed Ali <53947985+brainiac225@users.noreply.github.com>
2020-03-19 08:49:25 +0530

    Create sumofeven1s.c

---
+++ sumofeven1s.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<math.h>
+  
+int countOne(int n) 
+{ 
+    int count = 0; 
+    while (n) { 
+        n = n & (n - 1); 
+        count++; 
+    } 
+  
+    if (count % 2 == 0) 
+        return 1; 
+    else
+        return 0; 
+} 
+  
+int sumDigits(int n) 
+{ 
+    int sum = 0; 
+    while (n != 0) { 
+        sum += n % 10; 
+        n /= 10; 
+    } 
+  
+    return sum; 
+} 
+  
+int main() 
+{ 
+    int arr[] = { 4, 9, 15 }; 
+      
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    int total_sum = 0; 
+  
+    // Iterate through the array 
+    for (int i = 0; i < n; i++) { 
+        if (countOne(arr[i])) 
+            total_sum += sumDigits(arr[i]); 
+    } 
+  
+    printf("Sum is: %d", total_sum);
+      
+    return 0; 
+} 
